f61c12f7103cce9a2f431bda07ac08110677f06d
Kshitija Sharma <108922810+Kshitijasharma@users.noreply.github.com>
2023-11-02 10:29:09 +0530

    LL_deletion_function.cpp

--- LL_deletion_function.cpp
+++ LL_deletion_function.cpp
@@ -1,6 +1,26 @@
-////////////DELETION-function/////////////////////////////
+////////////Delete the first node///////////////
+void delete_at_head(node* &head)
+{
+    node* to_del_head=head;
+    head=head->link;
+    delete to_del_head;
+}
+
+
+////////////DELETION(this function will not work if we wanted to delete the 1st node of linked list)/////////////////////////////
 void deletion(node* &head,int value)
 {
+    if(head==NULL)
+    {
+        return ;
+
+    }
+    //head is the only one present
+    if(head->link==NULL)
+    {
+
+        return;
+    }
     node* temp=head;
     //traverse upto the value which is needed to be deleted
     while(temp->link->data!=value)
