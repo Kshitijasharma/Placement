4cfce0311ffce82f01fd06d3eb1bf7b4529e96c9
Kshitija Sharma <kshitijasharma01@gmail.com>
2023-12-14 19:33:58 +0530

    Update LL_insert_at_specific_posn.cpp

--- LL_insert_at_specific_posn.cpp
+++ LL_insert_at_specific_posn.cpp
@@ -1,25 +1,17 @@
-#include<iostream>
+// Online C++ compiler to run C++ program online
+#include <iostream>
 using namespace std;
-
-//class
 class node{
     public:
     int data;
-    struct node* link; //self referencing attribute
-
-    //constructor
-    node(int value){
+    node* link;
+    
+    node(int value)
+    {
         data=value;
         link=NULL;
     }
 };
-
-void insert_at_head(node* &head,int value)
-{
-    node* n= new node(value);
-    n->link=head;    //step 1: new node will access the address of the 1st node from the head
-    head=n;          //step 2: update the head and point it to the newly added node
-}
 //function to insert at tail
 void insert_at_tail(node* &head,int value)
 {
@@ -43,7 +35,38 @@ void insert_at_tail(node* &head,int value)
     temp->link=n;   //woh last node NULL rahegi so update it with the temp->link
 
 }
-
+//function to insert at specific position
+void insert_at_posn(node* &head,int value,int pos)
+{
+    node* n=new node(value);
+    
+    if(pos==0)
+    {
+        n->link=head;
+        head=n;
+    }
+    else
+    {
+        node* temp=head;
+        for(int i=0;i<pos-1 && temp!=NULL;i++)
+        {
+            temp=temp->link;
+            
+        }
+        if(temp!=NULL)
+        {
+            n->link=temp->link;
+            temp->link=n;
+            
+        }
+        else
+        {
+            cout<<"Invalid position"<<endl;
+            delete n;
+        }
+    }
+    
+}
 
 void display(node* head)
 {
@@ -57,43 +80,14 @@ void display(node* head)
     cout<<endl;
 }
 
-
-//////////////////////////////////////////////////
-// Function to insert a new node at a specific position
-    void insertAtPosition(node* &head,int value, int position) {
-        node* n = new node(value);
-        if (position == 0) {
-            n->link = head;
-            head = n;
-        }
-        else {
-            node* temp = head;
-            int currentPosition = 0;
-            while (temp != NULL && currentPosition < position - 1) {
-                temp = temp->link;
-                currentPosition++;
-            }
-            if (temp== nullptr) {
-                cout<< "Position out of bounds." << endl;
-                //delete n;
-                return;
-            }
-            n->link=temp->link;
-            temp->link= n;
-        }
-    }
-/////////////////////////////////////////////////
-
-int main()
-{
-    node* head=NULL;  //initially ll is empty
-    insert_at_head(head,0);
+int main() {
+    node* head=NULL;
     insert_at_tail(head,1);
-    insert_at_tail(head,2);
+    insert_at_tail(head,3);
+    insert_at_tail(head,4);
+    insert_at_posn(head,2,1);
     display(head);
-
-    insertAtPosition(head,100,2);
-    display(head);
+    
 
     return 0;
 }
